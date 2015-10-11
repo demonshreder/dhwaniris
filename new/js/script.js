04cbdf78a8750adacd07078b67f3f8fb15582db9
Anindya-Pandey <anindyapandey@gmail.com>
2015-10-11 17:34:34 +0530

    added partners hide and show

--- new/js/script.js
+++ new/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
             interval: 5000 //changes the speed
         })
         var i=1;
-        
+        var j=1;
         setInterval(function(){
                     if(i==1){
                         $('.batch1').show();
@@ -13,30 +13,100 @@ $(document).ready(function(){
                         $('.batch4').hide();
                         $('.batch5').hide();
                         //alert(i);
+                        if(j==1){
+                            $('.batch11').show();
+                            j=j+1;
+                        }
+                        else if(j==2){
+                            $('.batch22').show();
+                            $('.batch11').hide();
+                            j=j+1;
+                        }
+                        else if(j==3){
+                            $('.batch33').show();
+                            $('.batch22').hide();
+                            j=1;
+                        }
+                        
                         i = i+1;
                     }
                     else if(i==2){
                         $('.batch2').show();
                         $('.batch1').hide();
                         //alert(i);
+                        if(j==1){
+                            $('.batch11').show();
+                            j=j+1;
+                        }
+                        else if(j==2){
+                            $('.batch22').show();
+                            $('.batch11').hide();
+                            j=j+1;
+                        }
+                        else if(j==3){
+                            $('.batch33').show();
+                            $('.batch22').hide();
+                            j=1;
+                        }
                         i = i+1;
                     }
                     else if(i==3){
                         $('.batch3').show();
                         $('.batch2').hide();
                         //alert(i);
+                        if(j==1){
+                            $('.batch11').show();
+                            j=j+1;
+                        }
+                        else if(j==2){
+                            $('.batch22').show();
+                            $('.batch11').hide();
+                            j=j+1;
+                        }
+                        else if(j==3){
+                            $('.batch33').show();
+                            $('.batch22').hide();
+                            j=1;
+                        }
                         i = i+1;
                     }
                     else if(i==4){
                         $('.batch4').show();
                         $('.batch3').hide();
                         //alert(i);
+                        if(j==1){
+                            $('.batch11').show();
+                            j=j+1;
+                        }
+                        else if(j==2){
+                            $('.batch22').show();
+                            $('.batch11').hide();
+                            j=j+1;
+                        }
+                        else if(j==3){
+                            $('.batch33').show();
+                            $('.batch22').hide();
+                            j=1;
+                        }
                         i = i+1;
                     }
                     else if(i==5){
                         $('.batch5').show();
                         $('.batch4').hide();
-                        
+                        if(j==1){
+                            $('.batch11').show();
+                            j=j+1;
+                        }
+                        else if(j==2){
+                            $('.batch22').show();
+                            $('.batch11').hide();
+                            j=j+1;
+                        }
+                        else if(j==3){
+                            $('.batch33').show();
+                            $('.batch22').hide();
+                            j=1;
+                        }
                         i = 1;
                         //alert(i);
                     }

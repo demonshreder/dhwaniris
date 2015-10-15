2b0e83f2974eadd61fcdc4811705a588f47c0610
Anindya-Pandey <anindyapandey@gmail.com>
2015-10-16 00:14:46 +0530

    image transitions firefox

--- new/js/script.js
+++ new/js/script.js
@@ -4,118 +4,21 @@ $(document).ready(function(){
         $('.carousel').carousel({
             interval: 5000 //changes the speed
         })
-        var i=1;
-        var j=1;
+        var i=0;
         setInterval(function(){
-                    if(i==1){
-                        $('.batch1').show();
-                        $('.batch2').hide();
-                        $('.batch3').hide();
-                        $('.batch4').hide();
-                        $('.batch5').hide();
-                        //alert(i);
-                        if(j==1){
-                            $('.batch11').show();
-                            j=j+1;
-                        }
-                        else if(j==2){
-                            $('.batch22').show();
-                            $('.batch11').hide();
-                            j=j+1;
-                        }
-                        else if(j==3){
-                            $('.batch33').show();
-                            $('.batch22').hide();
-                            j=1;
-                        }
-                        
-                        i = i+1;
-                    }
-                    else if(i==2){
-                        $('.batch2').show();
-                        $('.batch1').hide();
-                        //alert(i);
-                        if(j==1){
-                            $('.batch11').show();
-                            j=j+1;
-                        }
-                        else if(j==2){
-                            $('.batch22').show();
-                            $('.batch11').hide();
-                            j=j+1;
-                        }
-                        else if(j==3){
-                            $('.batch33').show();
-                            $('.batch22').hide();
-                            j=1;
-                        }
-                        i = i+1;
-                    }
-                    else if(i==3){
-                        $('.batch3').show();
-                        $('.batch2').hide();
-                        //alert(i);
-                        if(j==1){
-                            $('.batch11').show();
-                            $('.batch33').hide();
-                            j=j+1;
-                        }
-                        else if(j==2){
-                            $('.batch22').show();
-                            $('.batch11').hide();
-                            j=j+1;
-                        }
-                        else if(j==3){
-                            $('.batch33').show();
-                            $('.batch22').hide();
-                            j=1;
-                        }
-                        i = i+1;
-                    }
-                    else if(i==4){
-                        $('.batch4').show();
-                        $('.batch3').hide();
-                        //alert(i);
-                        if(j==1){
-                            $('.batch11').show();
-                            $('.batch33').hide();
-                            j=j+1;
-                        }
-                        else if(j==2){
-                            $('.batch22').show();
-                            $('.batch11').hide();
-                            j=j+1;
-                        }
-                        else if(j==3){
-                            $('.batch33').show();
-                            $('.batch22').hide();
-                            j=1;
-                        }
-                        i = i+1;
-                    }
-                    else if(i==5){
-                        $('.batch5').show();
-                        $('.batch4').hide();
-                        if(j==1){
-                            $('.batch11').show();
-                            $('.batch33').hide();
-                            j=j+1;
-                        }
-                        else if(j==2){
-                            $('.batch22').show();
-                            $('.batch11').hide();
-                            j=j+1;
-                        }
-                        else if(j==3){
-                            $('.batch33').show();
-                            $('.batch22').hide();
-                            j=1;
-                        }
-                        i = 1;
-                        //alert(i);
-                    }
-            },2000);
-
+            var move = -80*i;
+            $('#partners').css("transform","translateX("+move+"px)");
+            $('#supports').css("transform","translateX("+move+"px)");
+            i=i+1;
+            if(move<0&&move%4000==0){
+                var hello = $('#span1').clone();
+                hello.insertAfter('#span1');
+            }
+            if(move<0&&move%2240==0){
+                var hello = $('#span2').clone();
+                hello.insertAfter('#span2');
+            }
+        },2000);
 //Code for fancybox
 $(".fancybox").fancybox({
     helpers : {

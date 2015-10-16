0147eca54de98ef88c7bfa98eda0432845f79d46
Anindya-Pandey <anindyapandey@gmail.com>
2015-10-17 00:19:30 +0530

    owl carousel

--- new/js/script.js
+++ new/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         $('.carousel').carousel({
             interval: 5000 //changes the speed
         })
-        var i=0;
+        /*var i=0;
         setInterval(function(){
             var move = -80*i;
             $('#partners').css("transform","translateX("+move+"px)");
@@ -18,7 +18,22 @@ $(document).ready(function(){
                 var hello = $('#span2').clone();
                 hello.insertAfter('#span2');
             }
-        },2000);
+        },2000);*/
+        $('.owl-carousel').owlCarousel({
+            margin:50,
+            loop:true,
+            autoWidth:true,
+            items:4,
+            animateOut: 'slideOutDown',
+            animateIn: 'flipInX',
+            stagePadding:50,
+            smartSpeed:450,
+            dots:true,
+            autoplay:true,
+            autoplayTimeout:1000,
+            autoplayHoverPause: true,
+            navigation:true
+        })
 //Code for fancybox
 $(".fancybox").fancybox({
     padding:0,
